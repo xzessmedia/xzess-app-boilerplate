29fa5d446e2caa0c6e73d53cc1136e7c018ac06c
xzessmedia <tim@xzessmedia.de>
2016-11-16 04:39:29 +0100

    included template page modification

--- js/application/config/config_routeprovider.js
+++ js/application/config/config_routeprovider.js
@@ -4,7 +4,7 @@ var app = angular.module('ApplicationApp');
 
 app.config(function($routeProvider) {
     $routeProvider
-        .when('/', { templateUrl: 'js/application/views/startpage.html' })
+        .when('/', { templateUrl: 'js/application/views/view_startpage.html' })
 
     /*
      * Add own Routes here like
